ab08d748fc8eac0085a58b22c26ddaba5a7f3a24
SebastianAndresen <sebastian.sommersted@live.dk>
2019-09-03 15:51:52 +0200

    Added a menu. Opens and closes on Escape. Buttons set the walk type

---
+++ BlindNight/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster instance;
+
+    public int walkType = 0;
+
+    public void Awake()
+    {
+        CreateGameMaster();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void CreateGameMaster()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetWalkType(int type)
+    {
+        walkType = type;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameObject filenamefld = null;
+            Transform[] trans = GameObject.Find("Canvas").GetComponentsInChildren<Transform>(true);
+            foreach (Transform t in trans)
+            {
+                if (t.gameObject.name == "MainMenu")
+                {
+                    filenamefld = t.gameObject;
+                    filenamefld.SetActive(!filenamefld.gameObject.activeSelf);
+                }
+            }
+        }
+    }
+}
