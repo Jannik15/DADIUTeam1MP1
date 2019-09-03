0eb41cd380c4ed688b745473c22ab6aecb4df187
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-09-03 10:24:04 +0200

    Script test

---
+++ BlindNight/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    // This is the script used to move the character.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
