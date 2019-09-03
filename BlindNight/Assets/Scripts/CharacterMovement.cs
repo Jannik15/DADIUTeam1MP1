d81def99e95851a425400b32d56e68016686a6c7
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-09-03 15:54:04 +0200

    Movement started

--- BlindNight/Assets/Scripts/CharacterMovement.cs
+++ BlindNight/Assets/Scripts/CharacterMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public int walkType, moveSpeed;
 
     // This is the script used to move the character.
     // Start is called before the first frame update
@@ -15,6 +16,23 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if(walkType == 0) {
+            //Touch touch = Input.GetTouch(0);
+            //transform.LookAt(touch.position);
+            //transform.position = transform.position * moveSpeed * touch.position;
+
+            bool aTouch = Input.GetMouseButtonDown(0);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if(aTouch /* && walkType == 0 */) {
+                if (Physics.Raycast(ray)) {
+                    Vector3 touchPos = Input.mousePosition;
+                    transform.position = touchPos * moveSpeed * Time.deltaTime; 
+            }
+          
+            }
+
+        //}
     }
 }
