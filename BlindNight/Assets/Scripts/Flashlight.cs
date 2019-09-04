a093d7dc100df8a32d7b8d0bf1f86dcb57a2a234
Jannik15 <jneerd15@student.aau.dk>
2019-09-04 17:21:31 +0200

    Flashlight wip

---
+++ BlindNight/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public GameObject volumetricLight;
+
+    void Update()
+    {
+        Debug.Log(volumetricLight.GetComponent<Material>().name);
+        //volumetricLight.GetComponent<Material>().color = gameObject.GetComponent<Light>().color;
+        //volumetricLight.GetComponent<Material>() = gameObject.GetComponent<Light>().color;
+    }
+}
