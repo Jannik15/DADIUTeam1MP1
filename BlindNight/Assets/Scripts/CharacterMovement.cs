c22628b46209aada87008e4a5040271aac9723e2
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-09-04 13:10:06 +0200

    Move 0 and 1

--- BlindNight/Assets/Scripts/CharacterMovement.cs
+++ BlindNight/Assets/Scripts/CharacterMovement.cs
@@ -4,35 +4,69 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public int walkType, moveSpeed;
 
+    private float playerMoveSpeed, moveThreshold, moveStep;
+    public int moveType;
+
+
+    
     // This is the script used to move the character.
     // Start is called before the first frame update
+
+    private Vector3 goalPos;
+    
     void Start()
     {
+        playerMoveSpeed = 10;
+        moveStep = playerMoveSpeed * Time.deltaTime;
+        goalPos = transform.position;
+        moveThreshold = 0.1f;
+        moveType = 0;
+    }
+
+    void Update()
+    {
+
+        GetInput();
+        if(Vector3.Distance(transform.position, goalPos) > moveThreshold)  {
+
+            transform.position = Vector3.MoveTowards(transform.position, goalPos, moveStep);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if(walkType == 0) {
-            //Touch touch = Input.GetTouch(0);
-            //transform.LookAt(touch.position);
-            //transform.position = transform.position * moveSpeed * touch.position;
+    public void GetInput(){
+        if(moveType == 0 && Input.GetMouseButtonDown(0)) {
 
-            bool aTouch = Input.GetMouseButtonDown(0);
-
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            
-            if(aTouch /* && walkType == 0 */) {
-                if (Physics.Raycast(ray)) {
-                    Vector3 touchPos = Input.mousePosition;
-                    transform.position = touchPos * moveSpeed * Time.deltaTime; 
-            }
-          
+                goalPos = getPressPos();
             }
 
-        //}
+/*             if(Input.GetTouch(0).phase == TouchPhase.Began) {
+                goalPos = getPressPos();
+            } */
+
+        if(moveType == 1 && Input.GetMouseButton(0)) {
+            //if(Input.touchCount > 1) {
+                goalPos = getPressPos();
+           // }
+        }
     }
+
+    public Vector3 getPressPos() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit)) {
+            return new Vector3(hit.point.x, transform.position.y, hit.point.z);
+      
+        }
+    
+        else {
+            return new Vector3(0,0,0);
+        }
+    }
+
 }
+
+
+
