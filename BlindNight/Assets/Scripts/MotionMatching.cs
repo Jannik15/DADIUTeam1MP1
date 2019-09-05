502690350e0f442a6e36051a6109af845728e0b0
Jannik15 <jneerd15@student.aau.dk>
2019-09-05 13:02:46 +0200

    Motion matching play animation

---
+++ BlindNight/Assets/Scripts/MotionMatching.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionMatching : MonoBehaviour
+{
+    public Transform[] rig;
+    CSVReader csvData;
+    int j = 0;
+    bool status = false;
+
+    void Start()
+    {
+        csvData = FindObjectOfType<CSVReader>();
+        Debug.Log(csvData.gameObject.name);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < rig.Length ; i++)
+        {
+            rig[i].rotation = csvData.GetQuaternions()[i][j];
+            rig[i].position = csvData.GetPositions()[i][j];
+        }
+
+        if (!status)
+            j++;
+        else
+            j--;
+        if (j >= csvData.GetQuaternions()[0].Count-1)
+            status = !status;
+    }
+}
+
+class TrajectoryPoint
+{
+
+}
